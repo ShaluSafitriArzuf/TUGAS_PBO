f2bac2092356e125b4fafff337c59a4790531dd6
Hp <Hp@DESKTOP-FEH4G3S>
2024-03-23 20:16:20 +0700

    tugas pbo

---
+++ src/tugaspbo/PolaBintang.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugaspbo;
+
+/**
+ *
+ * @author Hp
+ */
+public class PolaBintang {
+     //TUGAS NOMOR 1
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        int i, j, k;
+        for(i=0; i<=3; i++){
+            for(j=3; j>=i; j--){
+              System.out.print(" ");
+        }
+        for(k=0; k<=i; k++){
+         System.out.print("* ");
+        }System.out.println("");   
+}
+}
+}
+    
+    
+}
